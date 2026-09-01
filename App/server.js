63a2180899e6326e6233ff53c50f3f3490cdfee4
elofox226 <nyany456@gmail.com>
2026-09-01 19:21:52 +0800

    修正 GitHub Pages 路徑

--- App/server.js
+++ App/server.js
@@ -29,5 +29,5 @@ app.get("/books/:id", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
